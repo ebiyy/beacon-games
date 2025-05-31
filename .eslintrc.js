45222f8137ecc6e6a0059cf171e3d7ecd3f4717a
yusuke ebinuma <41263031+ebiyy@users.noreply.github.com>
2025-05-31 21:23:52 +0900

    build: ESLintとPrettierの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    'build',
+    '.next',
+    '.turbo',
+    '*.config.js',
+    '*.config.ts',
+  ],
+}
